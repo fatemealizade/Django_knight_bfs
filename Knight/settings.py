ffb1a3fae3bbc8b8bd69923f071efa35d142ad63
Fateme Alizade <Alizadeh.fatemeh@yahoo.com>
2021-02-14 04:03:37 +0330

    Rename app and change .gitignore

--- Knight/settings.py
+++ Knight/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'shortpath_knight',
+    'shortestpath_knight',
 ]
 
 MIDDLEWARE = [
--- shortpath_knight/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShortpathKnightConfig(AppConfig):
-    name = 'shortpath_knight'
