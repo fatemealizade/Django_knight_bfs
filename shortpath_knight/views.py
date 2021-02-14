0ed0f2a11a54ba429a8c4ded0240eb8d084e8616
Fateme Alizade <Alizadeh.fatemeh@yahoo.com>
2021-02-14 03:45:05 +0330

    Add logic to views.py

--- shortpath_knight/views.py
+++ shortpath_knight/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from itertools import product
+import math
+class knight:
+    def __init__(self, x, y, distance):
+        self.x = x
+        self.y = y
+        self.distance = distance
+    
+    
+def knight_bfs(curr_pos, target_pos, N):
+    
+    visited = [[False for i in range(N)] for j in range(N)]
+    queue = []
+    queue.append(knight(curr_pos[0], curr_pos[1], 0))
+    visited[curr_pos[0]][curr_pos[1]] = True
+    
+    while (len(queue) > 0):
+        top = queue.pop(0)
+        
+        for next_position in knight_moves(top, N):
+            if visited[next_position[0]][next_position[1]] == False:
+                queue.append(knight(next_position[0], next_position[1], top.distance + 1))
+                visited[next_position[0]][next_position[1]] = True
+            
+            if tuple(next_position) == tuple(target_pos):
+                return top.distance + 1
+    
+    return math.inf
 
-# Create your views here.
+
+def knight_moves(position, N):
+    x = position.x
+    y = position.y
+    moves = list(product([x-1, x+1], [y-2, y+2])) + list(product([x-2, x+2], [y-1, y+1]))
+    moves = [(x,y) for [x, y] in moves if x >= 0 and y >= 0 and x < N  and y < N]
+    return moves        
