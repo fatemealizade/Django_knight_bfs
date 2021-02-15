e0867d2645a8920822e1bdf6e530e8e9bb835b9a
Fateme Alizade <Alizadeh.fatemeh@yahoo.com>
2021-02-15 16:50:30 +0330

    Add request_inputes function

--- shortestpath_knight/views.py
+++ shortestpath_knight/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
 from itertools import product
 from django.views.decorators.csrf import csrf_exempt
+from django.http.response import HttpResponse
 import math
+import json
 class knight:
     def __init__(self, x, y, distance):
         self.x = x
         self.y = y
         self.distance = distance
     
-@csrf_exempt 
-def knight_bfs(curr_pos, target_pos, N):
+def request_inputes(request):
+    body_utf = request.body.decode('utf-8')
+    body = json.loads(body_utf)
+    curr_pos = []
+    target_pos = []
+    N = body["N"] 
     
+    curr_pos_x = body["knight_position_x"]
+    curr_pos_y = body["knight_position_y"]
+    
+    target_pos_x = body["knight_targetPosition_x"]
+    target_pos_y = body["knight_targetPosition_y"]
+    
+    curr_pos.extend((curr_pos_x, curr_pos_y))
+    target_pos.extend((target_pos_x, target_pos_y))
+    return (N, curr_pos, target_pos)
+
+def knight_bfs(curr_pos, target_pos, N):
     visited = [[False for i in range(N)] for j in range(N)]
     queue = []
     queue.append(knight(curr_pos[0], curr_pos[1], 0))
@@ -35,13 +52,4 @@ def knight_moves(position, N):
     y = position.y
     moves = list(product([x-1, x+1], [y-2, y+2])) + list(product([x-2, x+2], [y-1, y+1]))
     moves = [(x,y) for [x, y] in moves if x >= 0 and y >= 0 and x < N  and y < N]
-    return moves        
-
-if __name__ == '__main__':
-    N = int(input())
-    curr_pos_x = int(input()) - 1
-    curr_pos_y = int(input()) - 1
-    target_pos_x = int(input()) -1
-    target_pos_y = int(input()) -1
-    
-    print(knight_bfs([curr_pos_x, curr_pos_y], [target_pos_x, target_pos_y], N))  
+    return moves
