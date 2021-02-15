8f0a54c711c8950bce1485735622f358a1117b70
Fateme Alizade <Alizadeh.fatemeh@yahoo.com>
2021-02-15 19:50:59 +0330

    Add is_valid_position function

--- shortestpath_knight/views.py
+++ shortestpath_knight/views.py
@@ -1,6 +1,7 @@
 from itertools import product
 from django.views.decorators.csrf import csrf_exempt
 from django.http.response import HttpResponse
+from .config import *
 import math
 import json
 
@@ -24,7 +25,10 @@ class knight:
 @csrf_exempt
 def knight_game(request):
     N, curr_position, target_position = request_inputes(request)
-    return HttpResponse(knight_bfs(curr_position, target_position, N))
+    if is_valid_position(curr_position, N) and is_valid_position(target_position, N):
+        return HttpResponse(knight_bfs(curr_position, target_position, N))
+    else:
+        return HttpResponse(status=400)
 
 
 def request_inputes(request):
@@ -45,11 +49,15 @@ def request_inputes(request):
     return (N, curr_pos, target_pos)
 
 
+def is_valid_position(position, N):
+    return (position[X_INDEX] >= 0 and position[Y_INDEX] >= 0 and position[X_INDEX] < N and position[Y_INDEX] < N) 
+
+
 def knight_bfs(curr_pos, target_pos, N):
     visited = [[False for i in range(N)] for j in range(N)]
     queue = []
-    queue.append(knight(curr_pos[0], curr_pos[1], 0))
-    visited[curr_pos[0]][curr_pos[1]] = True
+    queue.append(knight(curr_pos[X_INDEX], curr_pos[Y_INDEX], 0))
+    visited[curr_pos[X_INDEX]][curr_pos[Y_INDEX]] = True
     
     if tuple(curr_pos) == tuple(target_pos):
         return 0
@@ -63,8 +71,8 @@ def knight_bfs(curr_pos, target_pos, N):
                 return top_adjacent_distance
             
             if visited[next_position[0]][next_position[1]] == False:
-                queue.append(knight(next_position[0], next_position[1], top_adjacent_distance))
-                visited[next_position[0]][next_position[1]] = True
+                queue.append(knight(next_position[X_INDEX], next_position[Y_INDEX], top_adjacent_distance))
+                visited[next_position[X_INDEX]][next_position[Y_INDEX]] = True
 
     return math.inf
 
@@ -73,5 +81,5 @@ def knight_moves(position, N):
     x = position.get_x()
     y = position.get_y()
     moves = list(product([x-1, x+1], [y-2, y+2])) + list(product([x-2, x+2], [y-1, y+1]))
-    moves = [(x,y) for [x, y] in moves if x >= 0 and y >= 0 and x < N  and y < N]
+    moves = [(x,y) for [x, y] in moves if is_valid_position([x, y], N)]
     return moves
